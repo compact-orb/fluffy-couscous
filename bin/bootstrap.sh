32bbb3fc26cc634ace835d530e858f091a89ea9d
compact-orb <compact-orb@outlook.com>
2026-09-26 18:33:13 +0800

    Save prog

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -7,19 +7,21 @@ source "$(realpath "$(dirname "${BASH_SOURCE[0]}")/../lib/common.sh")"
 seed_architecture="${1}"
 seed_name="${2}"
 profile="${3}"
+seed_dir="${work_dir}/seed"
+stage_dir="${work_dir}/stage"
 
 download_ebuild_repositories
 
-download_extract_latest_gentoo_autobuild "${work_dir}/seed" \
+download_extract_latest_gentoo_autobuild "${seed_dir}" \
     "${seed_architecture}" "${seed_name}"
 
-configure_ebuild_repositories "${work_dir}/seed"
+configure_ebuild_repositories "${seed_dir}"
 
-mount_ebuild_repositories "${work_dir}/seed"
+mount_ebuild_repositories "${seed_dir}"
 
-mount_chroot_filesystems "${work_dir}/seed"
+mount_chroot_filesystems "${seed_dir}"
 
-chroot "${work_dir}/seed" /usr/bin/bash --login -c \
+chroot "${seed_dir}" /usr/bin/bash --login -c \
     'emerge --jobs="$(nproc)" --deep --getbinpkg --newuse --update "@world"'
 
 if [[ "${4}" -eq "workaround" ]]; then
@@ -27,14 +29,14 @@ if [[ "${4}" -eq "workaround" ]]; then
     # without taking the time to update the seed's portage configuration
     # and re-merging the necessary packages.
 
-    create_file "${work_dir}/seed/etc/portage/package.use/clang-common" \
+    create_file "${seed_dir}/etc/portage/package.use/clang-common" \
         "llvm-core/clang-common default-lld"
-    create_file "${work_dir}/seed/etc/portage/package.use/clang-linker-config" \
+    create_file "${seed_dir}/etc/portage/package.use/clang-linker-config" \
         "llvm-core/clang-linker-config default-lld"
-    create_file "${work_dir}/seed/etc/portage/package.use/clang-runtime" \
+    create_file "${seed_dir}/etc/portage/package.use/clang-runtime" \
         "llvm-runtimes/clang-runtime default-lld polly"
     
-    chroot "${work_dir}/seed" /usr/bin/bash --login -c '
+    chroot "${seed_dir}" /usr/bin/bash --login -c '
         emerge --jobs="$(nproc)" --getbinpkg "llvm-core/clang" \
         "llvm-core/clang-common" "llvm-core/clang-linker-config" \
         "llvm-core/lld" "llvm-runtimes/clang-runtime"
@@ -43,12 +45,12 @@ if [[ "${4}" -eq "workaround" ]]; then
     # The custom profile might disable binutils-plugin for LLVM. Since
     # llvm-core/llvmgold requires binutils-plugin for LLVM, it has to be
     # unmerged.
-    chroot "${work_dir}/seed" /usr/bin/bash --login -c \
+    chroot "${seed_dir}" /usr/bin/bash --login -c \
         'emerge --unmerge "llvm-core/llvmgold"'
 
     # Clang binary package might not have abi_x86_32 forced, which might mean
     # that 32-bit symlinks were not created.
-    chroot "${work_dir}/seed" /usr/bin/bash --login -c '
+    chroot "${seed_dir}" /usr/bin/bash --login -c '
         LLVM_MAJOR=$(clang -dumpversion | cut --delimiter="." --fields="1")
         ln --force --symbolic "clang-${LLVM_MAJOR}" \
             "/usr/lib/llvm/${LLVM_MAJOR}/bin/i686-pc-linux-gnu-clang-${LLVM_MAJOR}"
@@ -57,6 +59,10 @@ if [[ "${4}" -eq "workaround" ]]; then
         '
 
     # Author's profile needs extra packages to be merged.
-    chroot "${work_dir}/seed" /usr/bin/bash --login -c \
+    chroot "${seed_dir}" /usr/bin/bash --login -c \
         'emerge --jobs="$(nproc)" --getbinpkg "net-misc/aria2"'
 fi
+
+
+
+unmount_chroot_filesystems "${seed_dir}"
